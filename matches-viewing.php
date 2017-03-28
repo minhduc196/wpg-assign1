ca7c09dead8ea26919c9b5a2e6cffdc8fe87b3b5
Minh Duc <minhducfit196@gmail.com>
2017-03-28 18:29:47 +0700

    Update search output

--- matches-viewing.php
+++ matches-viewing.php
@@ -1,9 +1,12 @@
 <?php include("top.html"); ?>
-<?php ini_set('error_reporting', 0) ?>
+<?php #ini_set('error_reporting', 0) ?>
+<?php include('common.php'); ?>
 <?php if(empty($_GET['name'])) {
 	header("Location: matches.php");
 	} ?>
 <?php $info = "";
+$err = "";
+$found = false;
 $file_name = "singles.txt";
 $user_name = $_GET['name'];
 $handle = file($file_name);
@@ -14,65 +17,78 @@ if(file_exists($file_name)) {
 			$info = $line;
 			$found = true;
 			break;
-		} else {
-			$found = false;
 		}
 	}
-} 
+} else { $err = "File not exist."; }
+
 ?>
 
 <div>
 	<strong>Matches for <?= $user_name; ?></strong>
 	
-	<?php 
-		//compare data to get matches
-	if($found) {
-		$info_array = explode(",", $info);
-		$person_gender = $info_array[1];
-		$person_age = $info_array[2];
-		$person_ptype = $info_array[3];
-		$person_os = $info_array[4];
-		$person_min_age = $info_array[5];
-		$person_max_age = $info_array[6];
-		foreach ($handle as $value) {
-			list($name, $gender, $age, $ptype, $os, $min_age, $max_age) = explode(",", $value);
+	<?php if(!$found) {
+		$err = "User not exist.";
+		} else {
+			$info_array = explode(',',$info);
+			$person_gender = $info_array[1];
+			$person_age = $info_array[2];
+			$person_ptype = $info_array[3];
+			$person_os = $info_array[4];
+			$person_min_age = $info_array[5];
+			$person_max_age = $info_array[6];
+			$match = false;
+			$num = 0;
 			similar_text($person_ptype, $ptype,$result);
-			if(($person_gender != $gender) && ($age >= $person_min_age && $age <= $person_max_age) 
-				&& (($person_age > $min_age) && ($person_age < $max_age))
-				&& ($person_os == $os) && ($result  >=25)) {
-			$dir = "img";
-		$loadFolder = opendir($dir.'/');
-		$user_image = preg_replace('/\s+/', '', $name);
-		$user_image_lc = md5(strtolower($user_image));
-		$file = scandir($dir);
-		$check = false;
-		foreach ($file as $match_image) {
-			if($user_image_lc == substr($match_image, 5, strlen($user_image_lc))) {
-				$avatar = $match_image;
-				$check = true;
-				break;
-			} else {
+			foreach ($handle as $key) {
+				list($name, $gender, $age, $ptype, $os, $min_age, $max_age) = explode(',',$key);
+				if(($person_gender != $gender) && ($age >= $person_min_age && $age <= $person_max_age) 
+					&& (($person_age > $min_age) && ($person_age < $max_age)) && ($person_os == $os) && ($result  >=25)) {
+					$match = true;
+					$match_name[] = $name;
+					$match_gender[] = $gender;
+					$match_age[] = $age;
+					$match_ptype[] = $ptype;
+					$match_os[] = $os;
+					$num++;		
+					$dir = "img";
+				$loadFolder = opendir($dir.'/');
+				$user_image = preg_replace('/\s+/', '', $name);
+				$user_image_lc = md5(strtolower($user_image));
+				$file = scandir($dir);
 				$check = false;
+				foreach ($file as $match_image) {
+					if($user_image_lc == substr($match_image, 5, strlen($user_image_lc))) {
+						$avatar = $match_image;
+						$check = true;
+						break;
+					} else {
+				}
+				}
+
+			if(!$check) {
+				$avatar = "user.jpg";
 			}
-		}
-		if(!$check) {
-			$avatar = "user.jpg";
-		}
-		$url = $dir."/". $avatar;
-	?>
+			$url = $dir."/". $avatar;
+					} else { $err = "No match found"; } 
+				} 
+			}
+			// die();
+			?>
+	<?php if($found && $match) {
+		for($i =0; $i < $num;$i++) {
+		?>
 	<div class="match">
-		<p><?= $name?></p>
-		<img src="<?php echo $url;?>" alt="user">
+		<p><?= $match_name[$i]?></p>
+		<img src="<?= $url;?>" alt="user">
 		<ul>
-			<li><strong>gender:</strong><?php echo $gender; ?></li>
-			<li><strong>age:</strong><?= $age?></li>
-			<li><strong>type:</strong><?php echo $ptype; ?></li>
-			<li><strong>OS:</strong><?php echo $os; ?></li>
+			<li><strong>gender:</strong><?= $match_gender[$i]; ?></li>
+			<li><strong>age:</strong><?= $match_age[$i]?></li>
+			<li><strong>type:</strong><?= $match_ptype[$i]; ?></li>
+			<li><strong>OS:</strong><?= $match_os[$i]; ?></li>
 		</ul>
 	</div>
-<?php }}} else {
-	echo "<br><strong>Username not found</strong>";
-	} ?>
+	<?php
+	}	} else { echo $err;} ?>
 </div>
 
 <?php include("bottom.html"); ?>
--- signup-submit.php
+++ signup-submit.php
@@ -1,68 +1,66 @@
 <?php include("top.html"); ?>
-<?php ini_set('error_reporting', 0) ?>
+<?php #ini_set('error_reporting', 0) ?>
 <?php if(empty($_POST['name'])) {
 	header("Location: signup.php");
-	} ?>
-<?php if(isset($_POST['name']) && !empty($_POST['name'])) {
-		$user_name = htmlentities($_POST['name']);
-		$user_gender = htmlentities($_POST['gender']);
-		$user_age = htmlentities($_POST['age']);
-		$user_ptype = htmlentities($_POST['ptype']);
-		$user_os = htmlentities($_POST['os']);
-		$user_min_age = htmlentities($_POST['min-age']);
-		$user_max_age = htmlentities($_POST['max-age']);
-		$detail = "";
-		$user_ptype_uc = strtoupper($user_ptype);
-		if(!preg_match("/[a-z^A-Z]/", $user_name)) {
-			echo "<a href=\"signup.php\">Go back</a><br>";
-			die($user_name . " is not a valid name.");
-		};
-		if(empty($user_gender)) {
-			echo "<a href=\"signup.php\">Go back</a><br>";
-			die("Gender is required");
-		};
-		if(!preg_match("/[1-9]/",$user_age) || empty($user_age)) {
-			echo "<a href=\"signup.php\">Go back</a><br>";
-			die($user_age . " is not a valid age");
-		};
-		if(!preg_match("/[I,E][N,S][F,T][J,P]/",$user_ptype) || empty($user_ptype)) {
-			echo "<a href=\"signup.php\">Go back</a><br>";
-			die($user_ptype . " is not a valid type");
-		};
-		if(empty($user_os)) {
-			echo "<a href=\"signup.php\">Go back</a><br>";
-			die("OS not valid");
-		}
-		if(!empty($user_max_age) && empty($user_min_age)) {
-			$user_min_age = 1;
-			$detail = "User min age not found, default value used.";
-		}
-		else if(!preg_match("/[1-9]/",$user_min_age) || empty($user_min_age)) {
-			echo "<a href=\"signup.php\">Go back</a><br>";
-			die($user_min_age . " is not a valid min age");
-		};
-		if(!empty($user_min_age) && empty($user_max_age)) {
-			$user_max_age = 99;
-			$detail = "User max age not found, default value used.";
-		}
-		else if(!preg_match("/[1-9]/",$user_max_age) || ($user_min_age > $user_max_age)) {
-			die($user_max_age . " is not a valid max age");
-		}
-	} else {
-		echo "<a href=\"signup.php\">Go back</a><br>";
-		die("All field must be filled");
-	}; ?>
+} ?>
+<?php include 'common.php';
+$err = "";
+$check = false; 
+if(isset($_POST['name']) && !empty($_POST['name'])) {
+	if(preg_match("/[a-z^A-Z]/", $_POST['name'])) {
+		$user_name = test_input($_POST['name']);
+		if(isset($_POST['gender'])) {
+			$user_gender = test_input($_POST['gender']);
+			if(isset($_POST['age']) && !empty($_POST['age'])) {
+				if(preg_match("/[1-9]/",$_POST['age'])) {
+					if($_POST['age'] >= 1 && $_POST['age'] <= 99) {
+						$user_age = test_input($_POST['age']);
+						if(isset($_POST['ptype']) && !empty($_POST['ptype'])) {
+							if(!preg_match("/[I,E][N,S][F,T][J,P]/",$_POST['ptype'])) {
+								$user_ptype = test_input($_POST['ptype']);
+								$user_ptype_uc = strtoupper($user_ptype);
+								if(isset($_POST['os'])) {
+									$user_os = test_input($_POST['os']);
+									if(isset($_POST['min-age']) && isset($_POST['max-age']) && !empty($_POST['min-age']) || !empty($_POST['max-age'])) {
+										if($_POST['max-age'] > $_POST['min-age']) {
+											$user_min_age = test_input($_POST['min-age']);
+											$user_max_age = test_input($_POST['max-age']);
+											$check = true;
+										} else { $err = "Min age must be smaller than max age."; };
+										if(empty($_POST['min-age'])) {
+											$user_min_age = 1;
+											$user_max_age = test_input($_POST['max-age']);
+											$check = true;
+										} else if(empty($_POST['max-age'])) {
+											$user_max_age = 99;
+											$user_min_age = test_input($_POST['min-age']);
+											$check = true;
+										} else if(empty($_POST['mix-age']) && empty($_POST['max-age'])) {
+											$check = false;
+											$err = "Both field cant be blank.";
+										} 
+									} else { $err = "At least one age field must be filled."; }
+								} else { $err = "No os selected."; }
+							} else { $err = "Invalid personal type."; }
+						} else { $err = "Personal type must be filled."; } 
+					} else { $err = "Age must be a number between 1 and 99."; }
+				} else { $err = "Age must be a number."; } 
+			} else { $err = "Age must be filled."; }
+		} else { $err = "Gender must be selected."; }
+	} else { $err = "Invalid name. Must be a character and number are not allowed."; } 
+} else { $err = "Name must be filled."; }
+?>
 
-	
+<?php if($check) { ?>
 
 <div>
 	<strong>Thank you</strong><br>
 	<p>Welcome to NerdLuv, <?php echo $user_name ?></p>
 	<p>Now <a href="matches.php">log in to see your matches.</a></p>
+</div>
 
-	<span style="color: red"><?= $detail?></span><br>
-
-	<?php 
+<?php 
+write_data($user_name, $user_gender, $user_age, $user_ptype_uc, $user_os, $user_min_age, $user_max_age);
 	$upload_file_name = $_FILES['user_img']['name'];
 	$upload_file_type = $_FILES['user_img']['type'];
 	$upload_file_size = $_FILES['user_img']['size'];
@@ -73,34 +71,19 @@
 	$tmp_upload_file_name = $_FILES['user_img']['tmp_name'];
 	$location = 'img/';
 	$file_type = 'image/jpeg';
-	
+
 	if(isset($upload_file_name)) {
-		if((($upload_file_extension=='jpg') || ($upload_file_extension=='jpeg'))
+		if((($upload_file_extension=='jpg') || ($upload_file_extension=='jpeg')) 
 			&& ($upload_file_type=='image/jpeg') && ($upload_file_size < $upload_file_max_size)) {
 			move_uploaded_file($tmp_upload_file_name,$location.$storage_file_name);
 		} else {
 			echo "No File uploaded.";
 		}
-	}; 
-	 ?>
-	
-	<?php
-		$file_name = "singles.txt"; 
-		if(file_exists($file_name)) {
-
-		// write data to file singles.txt
-		$openFile = fopen($file_name, "a");
-		 fopen("test.txt", "r+");
-		$txt = $user_name . "," . $user_gender . "," . $user_age . "," 
-		. $user_ptype . "," . $user_os . "," . $user_min_age . "," . $user_max_age . "\r\n";
-		fwrite($openFile, $txt);
-		fclose($openFile);	
-		
-	} else {
-		echo "File not exist to write.";
 	}
-	?>
-	
+ } else { ?>
+<div>
+	<strong>Sorry</strong><br>
+	<p><?php echo $err . " <a href=\"signup.php\">Go Back</a>";?></p>
 </div>
-
+<?php } ?>
 <?php include("bottom.html"); ?>
